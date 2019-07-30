b316a94524998cc5754e5b44933df9167ed60506
Cody Barr <cody.barr@gmail.com>
2019-07-29 23:32:43 -0400

    Testing Netlify Forms

---
+++ src/pages/Success.vue
@@ -0,0 +1,9 @@
+<template>
+	<Layout>
+		<div class="h-40 flex items-center justify-center">
+			<div class="flex-grow">
+				<p class="text-3xl text-center">Thanks for the message. I'll be in touch 😉</p>
+			</div>
+		</div>
+	</Layout>
+</template>
